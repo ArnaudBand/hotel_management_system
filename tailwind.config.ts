136b892607df87f68c5e32305dcce16a35f0f7b5
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-12-29 08:19:01 +0000

    Add colors and fontFamily to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
 const config: Config = {
   content: [
@@ -8,10 +9,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#038c7f',
+        secondary: '#f2c641',
+        tertiary: {
+          dark: '#f27405',
+          light: '#f2c641',
+        }
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)', ...fontFamily.sans],
       },
     },
   },
