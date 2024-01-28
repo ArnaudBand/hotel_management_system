1912203a6474727c9f02ae5ebf4baf5ad430e007
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-01-28 16:04:47 +0000

    Add account and booking schemas to schemaTypes array

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,5 @@
 import user from './user';
+import account from './account';
+import booking from './booking'
 
-export const schemaTypes = [user]
+export const schemaTypes = [user, account, booking]
