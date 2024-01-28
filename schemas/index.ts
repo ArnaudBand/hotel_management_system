8d6b8a42db664db9017206c90d7b547931d72bd5
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-01-28 16:36:38 +0000

    Add new schema types to index.ts

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,8 @@
 import user from './user';
 import account from './account';
-import booking from './booking'
+import booking from './booking';
+import hotelRoom from './hotelRoom';
+import review from './review';
+import verificationToken from './verificationToken';
 
-export const schemaTypes = [user, account, booking]
+export const schemaTypes = [user, account, booking, hotelRoom, review, verificationToken]
